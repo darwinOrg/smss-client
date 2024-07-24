69ebf78a350d460c1dd4d138089e0ec9aceddc52
chenbiao <149027310@qq.com>
2024-07-24 14:59:03 +0800

    refactor

--- sub.go
+++ sub.go
@@ -2,11 +2,15 @@ package client
 
 import (
 	"encoding/binary"
+	"errors"
 	"log"
+	"sync/atomic"
 	"time"
 )
 
 type AckEnum byte
+
+var TermiteError = errors.New("TermiteError")
 
 func (a AckEnum) Value() byte {
 	return byte(a)
@@ -20,8 +24,10 @@ const (
 
 type SubClient struct {
 	*network
-	mqName string
-	who    string
+	mqName           string
+	who              string
+	maxNoDataTimeout int64
+	termiteState     atomic.Bool
 }
 
 type MessagesAccept func(messages []*SubMessage) AckEnum
@@ -32,9 +38,10 @@ func NewSubClient(mqName, who, host string, port int, timeout time.Duration) (*S
 		return nil, err
 	}
 	return &SubClient{
-		network: nw,
-		mqName:  mqName,
-		who:     who,
+		network:          nw,
+		mqName:           mqName,
+		who:              who,
+		maxNoDataTimeout: 35 * 1000,
 	}, nil
 }
 
@@ -58,13 +65,32 @@ func (sc *SubClient) Sub(eventId int64, batchSize uint8, ackTimeout time.Duratio
 	return err
 }
 
+func (sc *SubClient) Termite(force bool) {
+	sc.termiteState.Store(true)
+	if force {
+		conn := sc.conn
+		if conn != nil {
+			conn.Close()
+		}
+	}
+}
+
 func (sc *SubClient) waitMessage(accept MessagesAccept) error {
 	var respHeader SubRespHeader
 	var err error
+	lastTime := time.Now().UnixMilli()
 	for {
 		if err = readAll(sc.conn, respHeader.buf[:], sc.ioTimeout); err != nil {
+			if sc.termiteState.Load() {
+				log.Printf("readAll met err, but termite:%v\n", err)
+				return TermiteError
+			}
 			if isTimeoutError(err) {
-				log.Printf("wait message timeout\n")
+				if time.Now().UnixMilli()-lastTime > sc.maxNoDataTimeout {
+					log.Printf("wait message timeout too long,maybe server dead\n")
+					return err
+				}
+				log.Printf("wait message timeout,continue...\n")
 				continue
 			}
 			return err
@@ -75,14 +101,17 @@ func (sc *SubClient) waitMessage(accept MessagesAccept) error {
 			return readErrCodeMsg(sc.conn, msgLen, sc.ioTimeout)
 		}
 
-		if code == AliveCode {
-			log.Printf("sub is alive\n")
-			continue
-		}
 		if code == SubEndCode {
 			log.Printf("peer notify to end,maybe mq deleted,end sub\n")
 			return nil
 		}
+
+		lastTime = time.Now().UnixMilli()
+		if code == AliveCode {
+			log.Printf("sub is alive\n")
+			continue
+		}
+
 		if code != OkCode {
 			log.Printf("not support response code\n")
 			return nil
@@ -126,7 +155,7 @@ func (sc *SubClient) calPackageSize() int {
 	return size
 }
 
-func (sc *SubClient) packageSubCmd(eventId int64, batchSize uint8, ackTimeout time.Duration) []byte {
+func (sc *SubClient) packageSubCmd(messageId int64, batchSize uint8, ackTimeout time.Duration) []byte {
 	buf := make([]byte, sc.calPackageSize())
 	buf[0] = CommandSub.Byte()
 	nameLen := len(sc.mqName)
@@ -138,7 +167,7 @@ func (sc *SubClient) packageSubCmd(eventId int64, batchSize uint8, ackTimeout ti
 	copy(next, sc.mqName)
 
 	next = next[nameLen:]
-	binary.LittleEndian.PutUint64(next, uint64(eventId))
+	binary.LittleEndian.PutUint64(next, uint64(messageId))
 	next = next[8:]
 	binary.LittleEndian.PutUint64(next, uint64(ackTimeout))
 	next = next[8:]
--- test/create/main.go
+++ test/create/main.go
@@ -24,7 +24,7 @@ func create() {
 	mqName := "order"
 
 	//expireAt := time.Now().Add(time.Minute * 2).UnixMilli()
-	err = pc.CreateMQ(mqName, 0, "tid-2209991")
+	err = pc.CreateTopic(mqName, 0, "tid-2209991")
 
 	log.Println(err)
 }
@@ -37,7 +37,7 @@ func delete() {
 	}
 	defer pc.Close()
 
-	err = pc.DeleteMQ("temp_mq", "tid-9999del33")
+	err = pc.DeleteTopic("temp_mq", "tid-9999del33")
 
 	log.Println(err)
 }
@@ -51,7 +51,7 @@ func getMqList() {
 	defer pc.Close()
 
 	var j string
-	j, err = pc.GetMqList("tid-99yymm009")
+	j, err = pc.GetTopicList("tid-99yymm009")
 
 	log.Println(j, err)
 }
